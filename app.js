29c22b359fc32730ca12c31677f9ce76fc137f19
Danladi <galadimad892@gmail.com>
2022-05-04 14:20:53 +0100

    added day mode and night mode

--- app.js
+++ app.js
@@ -9,5 +9,11 @@ setInterval(function() {
     let ss = day.getSeconds() * angle;
     hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
     min.style.transform = `rotateZ(${mm}deg)`;
-    sec.style.transform = `rotateZ(${ss}deg)`
+    sec.style.transform = `rotateZ(${ss}deg)`;
+    //night and day design control 
+    if (day.getHours() >= 6 && day.getHours() <= 19) {
+        document.body.style.background = "#92aba7";
+    } else {
+        document.body.style.background = " #303030";
+    }
 })
